e5c8d228141eae105983dd29ba00ecc56eb717d5
soyma20 <slaviksoyma2014@gmail.com>
2022-03-09 16:33:14 +0200

    class work

--- lesson 10/cw/cw.js
+++ lesson 10/cw/cw.js
@@ -47,16 +47,18 @@ button.addEventListener('click', function () {
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
+let hr = document.createElement('hr');
+document.body.appendChild(hr);
+
 let inputOne = document.createElement('input');
 let inputTwo = document.createElement('input');
 let inputThree = document.createElement('input');
 let buttonTwo = document.createElement('button');
 
 buttonTwo.innerText = 'Create sheet';
-
 document.body.append(inputOne,inputTwo,inputThree,buttonTwo);
 
-button.addEventListener('click', function () {
+buttonTwo.addEventListener('click', function () {
     let valueOne = inputOne.value;
     let valueTwo = inputTwo.value;
     let text = inputThree.value;
@@ -69,30 +71,71 @@ button.addEventListener('click', function () {
         conteiner.appendChild(table);
         document.body.appendChild(conteiner);
 
-        for (let i = 0; i < valueOne.length; i++) {
+        for (let i = 0; i < valueOne; i++) {
              let tr = document.createElement('tr');
              tr.style.border = '2px solid black';
-            for (let j = 0; j < valueTwo.length; j++) {
+            for (let j = 0; j < valueTwo; j++) {
                 let td = document.createElement('td');
                 td.style.border = '2px solid black';
                 td.innerText = `${text}`;
-                
-
+                table.appendChild(tr);
+                tr.appendChild(td);
             }
-
         }
     }
-
+    sheetGenerator(valueOne,valueTwo,text);
 })
+
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
-//
-//
+let hr2 = document.createElement('hr');
+document.body.appendChild(hr2);
+
+
+let badWords = ['fuck', 'shit', 'cunt'];
+let inputFilter = document.createElement('input');
+let buttonThree = document.createElement('button')
+buttonThree.innerText = 'check';
+document.body.append(inputFilter, buttonThree);
+
+buttonThree.addEventListener('click', function () {
+    let valueInput = inputFilter.value;
+
+    for (let word of badWords) {
+        if (word === valueInput) {
+            alert('Bad word!!!');
+            inputFilter.value = '';
+            return;
+        }
+    }
+
+    if (valueInput) {
+        alert('Nice)');
+        input.value = '';
+    }
+})
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+let hr3 = document.createElement('hr');
+document.body.appendChild(hr3);
+
+let badWords2 = ['fuck', 'shit', 'cunt'];
+let inputFilter2 = document.createElement('input');
+let buttonFour = document.createElement('button')
+buttonFour.innerText = 'check';
+document.body.append(inputFilter2, buttonFour);
+
+buttonFour.addEventListener('click', function (e) {
+    for (const word of badWords2) {
+        if (e.target.value.includes(word)){
+            alert('Bad word !!!');
+        }
+
+    }
+})
